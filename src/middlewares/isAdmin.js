e938f91abe275f6317e3ea7f347e7e9c9d6e0996
MuhammadHaqi_241401109 <haqyrasya21@gmail.com>
2025-12-10 09:21:09 +0700

    Update isAdmin.js

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -1,20 +1,17 @@
-// src/middlewares/isAdmin.js (FIXED CommonJS)
-
-// TIDAK ADA import verifyToken di sini! Kita asumsikan verifyToken dipanggil sebelumnya di route.
-
 function isAdmin(req, res, next) {
-    // Middleware ini hanya memeriksa req.user yang seharusnya sudah diisi oleh verifyToken
+   
     
-    // Cek apakah req.user ada (artinya token sudah diverifikasi) dan role-nya adalah 'admin'
+    
     if (req.user && req.user.role === 'admin') {
-        next(); // Lanjutkan ke route jika user adalah admin
+        next(); 
     } else {
-        // Tolak akses (403 Forbidden)
+        
         return res.status(403).json({ 
             message: "Akses ditolak. Diperlukan hak akses Admin." 
         });
     }
 }
 
-// Export fungsi tunggal
+
+
 module.exports = isAdmin;
