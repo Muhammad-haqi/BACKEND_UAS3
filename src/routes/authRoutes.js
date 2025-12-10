55c93b50409d2934851f6f3a977adfef44b740ee
MuhammadHaqi_241401109 <haqyrasya21@gmail.com>
2025-12-10 09:18:23 +0700

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,13 @@
-// src/routes/authRoutes.js (FINAL CommonJS)
+
 
 const express = require("express");
-// Menggunakan destructuring karena authController mengekspor objek
+
 const { register, login } = require("../controllers/authController.js"); 
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+
 
 module.exports = router;
